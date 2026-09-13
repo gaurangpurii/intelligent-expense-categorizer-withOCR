5a5e18cd022ed9093a5a6fd1671b68ecb08e7465
Gaurang Puri <gaurangpuri007@gmail.com>
2026-09-14 00:11:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,16 +6,11 @@ const nextConfig = {
       { protocol: 'https', hostname: 'avatars.githubusercontent.com', pathname: '/**' },
     ],
   },
-  experimental: {
-    // Remove if not using Server Components
-    serverComponentsExternalPackages: ['mongodb'],
-  },
   webpack(config, { dev }) {
     if (dev) {
-      // Reduce CPU/memory from file watching
       config.watchOptions = {
-        poll: 2000, // check every 2 seconds
-        aggregateTimeout: 300, // wait before rebuilding
+        poll: 2000,
+        aggregateTimeout: 300,
         ignored: ['**/node_modules'],
       };
     }
